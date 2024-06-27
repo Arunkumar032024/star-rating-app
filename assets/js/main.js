7160d5dee7c564d94a4d80ec63ea61ab7bfe9f3c
Arunkumar032024 <arunkumar032024@gmail.com>
2024-06-27 15:39:52 +0530

    Complete App

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,88 +1,88 @@
 $(document).ready(function(){
-    const user = {
-        name:'',
-        email:'',
-        mobile:'',
-        password:'',
-        confirmPassword:''
-    }
+    
 
     // code for star rating
-    let s1 = $('#s1');
-    let s2 = $('#s2');
-    let s3 = $('#s3');
-    let s4 = $('#s4');
-    let s5 = $('#s5');
-
     let rate = 0;
 
-    function starColor(color, ...stars){
-        stars.map(star => {
-            star.css('color', `${color}`);
-        })
-    }
-
-    s1.on({
+    $("#s1").on({
         'click': function (){
             rate = 1;   
-            console.log(rate)
-            starColor('rd', s1)
+            console.log(rate);
+            if(rate === 1){
+                $("#s1").css({color: "#F4CE14"})
+                $("#s2, #s3, #s4, #s5").css({color: "#45474B"})
+            }
         },
         'mouseover': function(){
-            starColor('#F4CE14', s1)
+            $("#s1").css({color: "#F4CE14"})
         },
         'mouseleave': function(){
-            rate != 1 ? starColor('#45474B', s1) : null ;
+            (rate !== 1) ? $("#s1").css({color: "#45474B"}) : null;
             
         },
         
     })
-    s2.on({
+    $("#s2").on({
         'click': function (){
             rate = 2;   
-            console.log(rate)
+            console.log(rate);
+            if(rate === 2){
+                $("#s1, #s2").css({color: "#F4CE14"})
+                $("#s3, #s4, #s5").css({color: "#45474B"})
+            }
         },
         'mouseover': function(){
-            starColor('#F4CE14', s1, s2);
+            $("#s1, #s2").css({color: "#F4CE14"})
         },
         'mouseleave': function(){
-            rate != 2 ?  starColor('#45474B', s1, s2) : null ;
+            (rate !== 2) ? $("#s1, #s2").css({color: "#45474B"}) : null;
         },
     })
-    s3.on({
+    $("#s3").on({
         'click': function (){
             rate = 3;   
             console.log(rate)
+            if(rate === 3){
+                $("#s1, #s2, #s3").css({color: "#F4CE14"})
+                $("#s4, #s5").css({color: "#45474B"})
+            }
         },
         'mouseover': function(){
-            starColor('#F4CE14', s1, s2, s3);
+            $("#s1, #s2, #s3").css({color: "#F4CE14"})
         },
         'mouseleave': function(){
-            rate != 3 ? starColor('#45474B', s1, s2, s3) : null ;
+            (rate !== 3) ? $("#s1, #s2, #s3").css({color: "#45474B"}) : null;
         },
     })
-    s4.on({
+    $("#s4").on({
         'click': function (){
             rate = 4;   
             console.log(rate)
+            if(rate === 4){
+                $("#s1, #s2, #s3, #s4").css({color: "#F4CE14"})
+                $("#s5").css({color: "#45474B"})
+            }
         },
         'mouseover': function(){
-            starColor('#F4CE14', s1, s2, s3, s4);
+            $("#s1, #s2, #s3, #s4").css({color: "#F4CE14"})
         },
         'mouseleave': function(){
-            rate != 4 ? starColor('#45474B', s1, s2, s3, s4) : null ;
+            (rate !== 4) ? $("#s1, #s2, #s3, #s4").css({color: "#45474B"}) : null;
         },
     })
-    s5.on({
+    $("#s5").on({
         'click': function (){
             rate = 5;   
             console.log(rate)
+            if(rate === 5){
+                $("#s1, #s2, #s3, #s4, #s5").css({color: "#F4CE14"})
+            }
         },
         'mouseover': function(){
-            starColor('#F4CE14', s1, s2, s3, s4, s5);
+            $("#s1, #s2, #s3, #s4, #s5").css({color: "#F4CE14"})
         },
         'mouseleave': function(){
-            rate != 5 ? starColor('#45474B', s1, s2, s3, s4, s5) : null ;
+            (rate !== 5) ? $("#s1, #s2, #s3, #s4, #s5").css({color: "#45474B"}) : null;
         },
     })
 
